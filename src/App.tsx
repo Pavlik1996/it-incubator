072a40d110168c3a65d2e1d9d3d85a33e0c4cd5b
Pavel Khrysto <miki2012miki@gmail.com>
2022-11-29 17:22:03 +0300

    test laptop2

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>test</p>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
